a10b002b5afb5f3981ef79c214e219ea30325b88
Your Name <youremail@yourdomain.com>
2022-09-29 11:11:31 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: use_build_context_synchronously
+
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application/login_page.dart';
 import 'package:flutter_application/splash_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -13,5 +11,3 @@ void main() {
     home: const SplashScreen(),
   ));
 }
-
-
