c5f79e266f01355a27099c1f709ef3b3cfcac0b6
Your Name <youremail@yourdomain.com>
2022-10-17 23:47:06 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,3 @@ void main() async {
   ));
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
-      ),
-      body: Container(
-        child: const Text("data"),
-      ),
-    );
-  }
-}
